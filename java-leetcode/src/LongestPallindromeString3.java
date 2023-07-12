323086f6eb72fc6f8bdcc47881e91f83d3af2b75
Sanu07 <whyalwaysme07@gmail.com>
2023-07-12 22:08:12 +0530

    longest pallindromic string

---
+++ java-leetcode/src/LongestPallindromeString3.java
@@ -0,0 +1,34 @@
+public class LongestPallindromeString3 {
+    public static void main(String[] args) {
+        String str = "dabcba";
+        if (str.isEmpty()) {
+            System.out.println("");
+            return;
+        }
+
+        if (str.length() == 1) {
+            System.out.println(str);
+            return;
+        }
+
+        int longest = 0;
+        String longestStr = null;
+
+        for (int i = 0; i < str.length(); i++) {
+            String temp = helper(str, i, i);
+            if (temp.length() > longest) {
+                longestStr = temp;
+                longest = temp.length();
+            }
+        }
+        System.out.println(longestStr);
+    }
+
+    private static String helper(String str, int begin, int end) {
+        while (begin >= 0 && end < str.length() && str.charAt(begin) == str.charAt(end)) {
+            begin--;
+            end++;
+        }
+        return str.substring(begin + 1, end);
+    }
+}
