fbd079cb37df32ed91bca5bbb4ed982a69e3f054
Sanu07 <whyalwaysme07@gmail.com>
2023-07-16 22:34:16 +0530

    Create P13TwoSumAddFind.java

---
+++ java-leetcode/src/P13TwoSumAddFind.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class P13TwoSumAddFind {
+
+    private static List<Integer> list = new ArrayList<>();
+    public static void main(String[] args) {
+        add(2);
+        add(7);
+        add(11);
+        add(15);
+        find(27);
+        find(9);
+    }
+
+    private static void add(int value) {
+        list.add(value);
+    }
+
+    private static void find(int value) {
+        Set<Integer> set = new HashSet<>();
+        for (Integer integer : list) {
+            if (set.contains(value - integer)) {
+                System.out.println(true);
+                return;
+            } else {
+                set.add(integer);
+            }
+        }
+        System.out.println(false);
+    }
+}
