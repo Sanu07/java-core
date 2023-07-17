aa31c157058a914169b535e867f37b47e35ae833
Sanu07 <whyalwaysme07@gmail.com>
2023-07-17 23:15:01 +0530

    Create P37ReverseWord.java

---
+++ java-leetcode/src/P37ReverseWord.java
@@ -0,0 +1,16 @@
+public class P37ReverseWord {
+    public static void main(String[] args) {
+        String s = "the sky is blue";
+        if (s.isEmpty()) {
+            System.out.println(s);
+            return;
+        }
+        final String[] words = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]).append(" ");
+        }
+        final String result = sb.substring(0, sb.length() - 1);
+        System.out.println(result);
+    }
+}
