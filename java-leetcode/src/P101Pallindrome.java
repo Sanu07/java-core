eb90289a8ecadceedac6b52e923bd61a11bd42aa
Sanu07 <whyalwaysme07@gmail.com>
2023-07-18 23:44:29 +0530

    Create P101Pallindrome.java

---
+++ java-leetcode/src/P101Pallindrome.java
@@ -0,0 +1,18 @@
+public class P101Pallindrome {
+    public static void main(String[] args) {
+        int x = 14491;
+
+        int temp = x;
+        int result = 0;
+        while (temp != 0) {
+            int rem = temp % 10;
+            result = result * 10 + rem;
+            temp = temp / 10;
+        }
+        if (result == x) {
+            System.out.println("Pallindrome");
+        } else {
+            System.out.println("Not Pallindrome");
+        }
+    }
+}
